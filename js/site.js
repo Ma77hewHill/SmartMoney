22274d1c3d49f8601661546fc070470c8716a083
Ma77hewHill <61993268+Ma77hewHill@users.noreply.github.com>
2023-02-02 14:27:40 +0000

    JS started

--- js/site.js
+++ js/site.js
@@ -1,4 +1,28 @@
-//Call Hello World 
-function HelloWorld(){
-    alert("Hello World");
+//get values from the user 
+function getValues(){
+    //Get the values from the user 
+    let loanAmount = document.getElementById("amount").value;
+    let loanTerm = document.getElementById("term").value;
+    let interestRate = document.getElementById("rate").value;
+
+        //parse for numbers 
+        loanAmount = parseInt(loanAmount);
+        loanTerm = parseInt(loanAmount);
+        interestRate = parseInt(interestRate);
+    
+        //check that they are integers 
+        if(Number.isInteger(loanAmount) && Number.isInteger(loanTerm) && Number.isInteger(interestRate)){
+            //call calculator
+            let smArray = calculator(loanAmount, loanTerm, interestRate);
+            //call displayData
+            displayData(smArray);
+        }else{
+            alert("You Must Enter Numbers");
+        } 
+}
+
+function calculator(loanAmount,loanTerm,interestRate){
+      let returnArray = [];
+      
+      
 }
